50430f1c82e0c589c49e909373e17063dc8dca59
rum2mojito <wead3105@sjtu.edu.cn>
2019-08-02 12:24:24 +0800

    add constant

---
+++ lab3/code/Constant.v
@@ -0,0 +1,43 @@
+// A073708 YUWEI, SHIH
+// *****global variables*****
+`define ZeroWord 32'h00000000
+
+// OP code
+`define R_FORMAT 6'b000000
+`define ADDI 6'b001000
+`define SLTI 6'b001010
+`define BEQ 6'b000100
+`define LW 6'b100011
+`define SW 6'b101011
+`define JUMP 6'000010
+
+// ALU OP
+`define ALU_OP_R 3'b010
+`define ALU_OP_ADDI 3'b100
+`define ALU_OP_SLTI 3'b101
+`define ALU_OP_BEQ 3'b001
+`define ALU_OP_LW_SW 3'b000
+
+// shamt
+`define NOT_SHIFT 5'b00000
+
+// ALU FUNCT
+`define ALU_FUNCT_ADD 6'b100000
+`define ALU_FUNCT_SUB 6'b100010
+`define ALU_FUNCT_AND 6'b100100
+`define ALU_FUNCT_OR 6'b100101
+`define ALU_FUNCT_SLT 6'b101010
+
+// ALU CTRL
+`define AND_CTRL 4'b0000
+`define OR_CTRL 4'b0001
+`define ADD_CTRL 4'b0010
+`define SUB_CTRL 4'b0110
+`define NOR_CTRL 4'b1100
+`define SLT_CTRL 4'b0111
+
+// ALU code
+`define AND_ALU 2'b00
+`define OR_ALU 2'b01
+`define ADD_ALU 2'b10
+`define SLT_ALU 2'b11
--- lab3/code/Decoder.v
+++ lab3/code/Decoder.v
@@ -17,7 +17,12 @@ module Decoder(
 	ALU_op_o,
 	ALUSrc_o,
 	RegDst_o,
-	Branch_o
+	Branch_o,
+	Jump_o,
+	DataWrite_o,
+	MemRead_o,
+	MemWrite_o,
+	BranchType_o
 	);
      
 //I/O ports
@@ -28,6 +33,11 @@ output [3-1:0] ALU_op_o;
 output         ALUSrc_o;
 output         RegDst_o;
 output         Branch_o;
+output         Jump_o;
+output         DataWrite_o;
+output         MemRead_o;
+output         MemWrite_o;
+output         BranchType_o;
  
 //Internal Signals
 reg    [3-1:0] ALU_op_o;
@@ -35,6 +45,11 @@ reg            ALUSrc_o;
 reg            RegWrite_o;
 reg            RegDst_o;
 reg            Branch_o;
+reg            Jump_o;
+reg            DataWrite_o;
+reg            MemRead_o;
+reg            MemWrite_o;
+reg            BranchType_o;
 
 //Parameter
 
@@ -47,30 +62,88 @@ always@(*) begin
 		RegWrite_o <= 1'b1;
 		RegDst_o <= 1'b1;
 		Branch_o <= 1'b0;
+		Jump_o <= 1'b1;
+		DataWrite_o <= 1'b0;
+		MemRead_o <= 1'b0;
+		MemWrite_o <= 1'b0;
+		BranchType_o <= 1'bx;
 	end else if(instr_op_i == `ADDI) begin
 		ALU_op_o <= `ALU_OP_ADDI;
 		ALUSrc_o <= 1'b1;
 		RegWrite_o <= 1'b1;
 		RegDst_o <= 1'b0;
 		Branch_o <= 1'b0;
+		Jump_o <= 1'b1;
+		DataWrite_o <= 1'b0;
+		MemRead_o <= 1'b0;
+		MemWrite_o <= 1'b0;
+		BranchType_o <= 1'bx;
 	end else if(instr_op_i == `SLTI) begin
 		ALU_op_o <= `ALU_OP_SLTI;
 		ALUSrc_o <= 1'b1;
 		RegWrite_o <= 1'b1;
 		RegDst_o <= 1'b0;
 		Branch_o <= 1'b0;
+		Jump_o <= 1'b1;
+		DataWrite_o <= 1'b0;
+		MemRead_o <= 1'b0;
+		MemWrite_o <= 1'b0;
+		BranchType_o <= 1'bx;
 	end else if(instr_op_i == `BEQ) begin
 		ALU_op_o <= `ALU_OP_BEQ;
 		ALUSrc_o <= 1'b0;
 		RegWrite_o <= 1'b0;
 		RegDst_o <= 1'b0;
 		Branch_o <= 1'b1;
+		Jump_o <= 1'b1;
+		DataWrite_o <= 1'b0;
+		MemRead_o <= 1'b0;
+		MemWrite_o <= 1'b0;
+		BranchType_o <= 1'bx;
+	end else if(instr_op_i == `LW) begin
+		ALU_op_o <= `ALU_OP_LW_SW;
+		ALUSrc_o <= 1'b1;
+		RegWrite_o <= 1'b1;
+		RegDst_o <= 1'b1;
+		Branch_o <= 1'b0;
+		Jump_o <= 1'b1;
+		DataWrite_o <= 1'b0;
+		MemRead_o <= 1'b1;
+		MemWrite_o <= 1'b0;
+		BranchType_o <= 1'bx;
+	end else if(instr_op_i == `SW) begin
+		ALU_op_o <= `ALU_OP_LW_SW;
+		ALUSrc_o <= 1'b1;
+		RegWrite_o <= 1'b0;
+		RegDst_o <= 1'bx;
+		Branch_o <= 1'b0;
+		Jump_o <= 1'b1;
+		DataWrite_o <= 1'b1;
+		MemRead_o <= 1'b0;
+		MemWrite_o <= 1'b1;
+		BranchType_o <= 1'bx;
+	end else if(instr_op_i == `JUMP) begin
+		ALU_op_o <= 3'bxxx;
+		ALUSrc_o <= 1'bx;
+		RegWrite_o <= 1'bx;
+		RegDst_o <= 1'bx;
+		Branch_o <= 1'bx;
+		Jump_o <= 1'b0;
+		DataWrite_o <= 1'b0;
+		MemRead_o <= 1'b0;
+		MemWrite_o <= 1'b0;
+		BranchType_o <= 1'bx;
 	end else begin
 		ALU_op_o <= 3'bxxx;
 		ALUSrc_o <= 1'bx;
 		RegWrite_o <= 1'bx;
 		RegDst_o <= 1'bx;
 		Branch_o <= 1'bx;
+		Jump_o <= 1'bx;
+		DataWrite_o <= 1'b0;
+		MemRead_o <= 1'b0;
+		MemWrite_o <= 1'b0;
+		BranchType_o <= 1'bx;
 	end
 end
 
